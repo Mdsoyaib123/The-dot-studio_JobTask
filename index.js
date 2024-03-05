ea5f1a0368dbeb84ba96924ad9977038c17b548a
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-03-06 01:11:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function card1() {
+  const card1Container = document.getElementById("cardOne");
+  const img = document.createElement("img");
+  img.src =
+    "https://i.postimg.cc/BQ4jBsCs/image1-1.jpg";
+  card1Container.appendChild(img);
+}
+card1();
+
+function card2() {
+  const card2Container = document.getElementById("cardTwo");
+  const img = document.createElement("img");
+  img.src =
+    "https://i.postimg.cc/HkW7dHJ3/image2-2-1.jpg";
+  card2Container.appendChild(img);
+}
+card2();
+
+function card3() {
+  const card3Container = document.getElementById("cardThree");
+  const img = document.createElement("img");
+  img.src =
+    "https://i.postimg.cc/9frRwpY7/image3-1.jpg";
+  card3Container.appendChild(img);
+}
+card3();
+
+function card4() {
+  const card4Container = document.getElementById("cardFour");
+  const img = document.createElement("img");
+  img.src =
+    "https://i.postimg.cc/L6KtdrB0/image4-1.jpg";
+  card4Container.appendChild(img);
+}
+card4();
